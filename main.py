d92c17a887fd79cf5d467239fb219b7d8ef3d3b8
Nhut Vo <nhutmvo1997@gmail.com>
2020-12-13 22:09:25 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from Model.fv_main import fv_run
 from Model.ov_main import ov_run
 
 if __name__ == '__main__':
-    # ov_run()
+    ov_run()
     print('======================================================================')
     fv_run()
 
